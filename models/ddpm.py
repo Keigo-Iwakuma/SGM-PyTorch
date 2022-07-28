0220738afd0cfa229e655f6fcd337ebe25aac939
Keigo-Iwakuma <sym90chn@gmail.com>
2022-07-29 04:08:30 +0900

    upsate models/ddpm.py.

--- models/ddpm.py
+++ models/ddpm.py
@@ -8,3 +8,13 @@ import torch.nn as nn
 import functools
 
 from . import utils, layers, normalization
+
+RefineBlock = layers.RefineBlock
+ResidualBlock = layers.ResidualBlock
+ResnetBlockDDPM = layers.ResnetBlockDDPM
+Upsample = layers.Upsample
+Downsample = layers.Downsample
+conv3x3 = layers.ddpm_conv3x3
+get_act = layers.get_act
+get_normalization = normalization.get_normalization
+default_initializer = layers.default_init
