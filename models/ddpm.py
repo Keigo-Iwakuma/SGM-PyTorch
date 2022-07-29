56e25bd9fbb92fa57848efdd38e62a6b609246f0
Keigo-Iwakuma <sym90chn@gmail.com>
2022-07-29 23:58:56 +0900

    update models/ddpm.py.

--- models/ddpm.py
+++ models/ddpm.py
@@ -18,3 +18,24 @@ conv3x3 = layers.ddpm_conv3x3
 get_act = layers.get_act
 get_normalization = normalization.get_normalization
 default_initializer = layers.default_init
+
+
+@utils.register_model(name="ddpm")
+class DDPM(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.act = act = get_act(config)
+        self.register_buffer("sigmas", torch.tensor(utils.get_sigmas(config)))
+
+        self.nf = nf = config.model.nf
+        ch_mult = config.model.ch_mult
+        self.num_res_blocks = num_res_blocks = config.model.num_res_blocks
+        self.attn_resolutions = attn_resolutions = config.model.attn_resolutions
+        dropout = config .model.dropout
+        resamp_with_conf = config.model.resamp_with_conv
+        self.num_resolutions = num_resolutions = len(ch_mult)
+        self.all_resolutions = all_resolutions = [config.data.image_size // (2 ** i) for i in range(num_resolutions)]
+
+        AttnBlock = functools.partial(layers.AttnBlock)
+        self.conditional = conditional = config.model.conditional
+        ResnetBlock = functools.partial(ResnetBlockDDPM, act=act, temb_dim=4 * nf, dropout=dropout)
