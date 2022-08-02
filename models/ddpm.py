14de0cace50d99ad801fd67d7552c33989d5f1e6
Keigo-Iwakuma <sym90chn@gmail.com>
2022-08-02 23:52:05 +0900

    update models/ddpm.py.

--- models/ddpm.py
+++ models/ddpm.py
@@ -32,10 +32,61 @@ class DDPM(nn.Module):
         self.num_res_blocks = num_res_blocks = config.model.num_res_blocks
         self.attn_resolutions = attn_resolutions = config.model.attn_resolutions
         dropout = config .model.dropout
-        resamp_with_conf = config.model.resamp_with_conv
+        resamp_with_conv = config.model.resamp_with_conv
         self.num_resolutions = num_resolutions = len(ch_mult)
         self.all_resolutions = all_resolutions = [config.data.image_size // (2 ** i) for i in range(num_resolutions)]
 
         AttnBlock = functools.partial(layers.AttnBlock)
         self.conditional = conditional = config.model.conditional
         ResnetBlock = functools.partial(ResnetBlockDDPM, act=act, temb_dim=4 * nf, dropout=dropout)
+        if conditional:
+            # Condition on noise levels.
+            modules = [nn.Linear(nf, nf * 4)]
+            modules[0].weight.data = default_initializer()(modules[0].weight.data.shape)
+            nn.init.zeros_(modules[0].bias)
+            modules.append(nn.Linear(nf * 4, nf * 4))
+            modules[1].weight.data = default_initializer()(modules[1].weight.data.shape)
+            nn.init.zeros_(modules[1].bias)
+        
+        self.centered = config.data.centered
+        channels = config.data.num_channels
+
+        # Downsampling block
+        modules.append(conv3x3(channels, nf))
+        hs_c = [nf]
+        in_ch = nf
+        for i_level in range(num_resolutions):
+            # Residual blocks for this resolution
+            for i_block in range(num_res_blocks):
+                out_ch = nf * ch_mult[i_level]
+                modules.append(ResnetBlock(in_ch=in_ch, out_ch=out_ch))
+                in_ch = out_ch
+                if all_resolutions[i_level] in attn_resolutions:
+                    modules.append(AttnBlock(channels=in_ch))
+                hs_c.append(in_ch)
+            if i_level != num_resolutions - 1:
+                modules.append(Downsample(channels=in_ch, with_conv=resamp_with_conv))
+                hs_c.append(in_ch)
+        
+        in_ch = hs_c[-1]
+        modules.append(ResnetBlock(in_ch=in_ch))
+        modules.append(AttnBlock(channels=in_ch))
+        modules.append(ResnetBlock(in_ch=in_ch))
+
+        # Upsampling block
+        for i_level in reversed(range(num_resolutions)):
+            for i_block in range(num_res_blocks + 1):
+                out_ch = nf * ch_mult[i_level]
+                modules.append(ResnetBlock(in_ch=in_ch + hs_c.pop(), out_ch=out_ch))
+                in_ch = out_ch
+            if all_resolutions[i_level] in attn_resolutions:
+                modules.append(AttnBlock(channels=in_ch))
+            if i_level != 0:
+                modules.append(Upsample(channels=in_ch, with_conv=resamp_with_conv))
+        
+        assert not hs_c
+        modules.append(nn.GroupNorm(num_channels=in_ch, num_groups=32, eps=1e-6))
+        modules.append(conv3x3(in_ch, channels, init_scale=0.))
+        self.all_modules = nn.ModuleList(modules)
+
+        self.scale_by_sigma = config.model.scale_by_sigma
