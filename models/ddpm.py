f17cc8311df83363d7e90c93efd403e6ea1eb5de
Keigo-Iwakuma <sym90chn@gmail.com>
2022-08-03 01:47:51 +0900

    complete models/ddpm.py.

--- models/ddpm.py
+++ models/ddpm.py
@@ -90,3 +90,76 @@ class DDPM(nn.Module):
         self.all_modules = nn.ModuleList(modules)
 
         self.scale_by_sigma = config.model.scale_by_sigma
+    
+    def forward(self, x, labels):
+        modules = self.all_modules
+        m_idx = 0
+        if self.conditional:
+            # timestep/scale embedding
+            timesteps = labels
+            temb = layers.get_timestep_embedding(timesteps, self.nf)
+            temb = modules[m_idx](temb)
+            m_idx += 1
+            temb = modules[m_idx](self.act(temb))
+            m_idx += 1
+        else:
+            temb = None
+        
+        if self.centered:
+            # Input is in [-1, 1]
+            h = x
+        else:
+            # Input is in [0, 1]
+            h = 2 * x - 1.
+        
+        # Downsampling block
+        hs = [modules[m_idx][(h)]]
+        m_idx += 1
+        for i_level in range(self.num_resolutions):
+            # Residual blocks for this resolution
+            for i_block in range(self.num_res_blocks):
+                h = modules[m_idx](hs[-1], temb)
+                m_idx += 1
+                if h.shape[-1] in self.attn_resolutions:
+                    h = modules[m_idx](h)
+                    m_idx += 1
+                if i_level != self.num_resolutions - 1:
+                    hs.append(modules[m_idx](hs[-1]))
+                    m_idx += 1
+
+        h = hs[-1]
+        h = modules[m_idx](h, temb)
+        m_idx += 1
+        h = modules[m_idx](h)
+        m_idx += 1
+        h = modules[m_idx](h, temb)
+        m_idx += 1
+
+        # Upsampling block
+        for i_level in reversed(range(self.num_resolutions)):
+            for i_block in range(self.num_res_blocks + 1):
+                h = modules[m_idx](torch.cat([h, hs.pop()], dim=1), temb)
+                m_idx += 1
+            if h.shape[-1] in self.attn_resolutions:
+                h = modules[m_idx](h)
+                m_idx += 1
+            if i_level != 0:
+                h = modules[m_idx](h)
+                m_idx += 1
+        
+        assert not hs
+        h = self.act(modules[m_idx](h))
+        m_idx += 1
+        h = modules[m_idx](h)
+        m_idx += 1
+        assert m_idx == len(modules)
+
+        if self.scale_by_sigma:
+            # Devide the output by sigmas. Useful for training with the NCSN loss.
+            # The DDPM loss scales the network output by sigma in the loss function,
+            # so no need of doing it here.
+            used_sigmas = self.sigmas[labels, None, None, None]
+            h = h / used_sigmas
+        
+        return h
+
