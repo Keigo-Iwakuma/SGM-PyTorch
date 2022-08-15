ad2563468b1bdc0d98c01e4baa04bc567fbe44a4
Keigo-Iwakuma <sym90chn@gmail.com>
2022-08-15 23:50:56 +0900

    update models/up_or_down_sampling.py.

--- models/up_or_down_sampling.py
+++ models/up_or_down_sampling.py
@@ -10,3 +10,13 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 from op import upfirdn2d
+
+
+# Function ported from StyleGAN2
+def get_weight(module, shape, weight_var="weight", kernel_init=None):
+    """Get/create weight tensor for a convolution or fully-connected layer."""
+    return module.param(weight_var, kernel_init, shape)
+
+
+class Conv2d(nn.Module):
+    """Conv2d layer with optimal upsampling and downsampling (StyleGAN2)."""
