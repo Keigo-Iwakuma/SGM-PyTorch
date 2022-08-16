3ea5ec63a11ef7e28c082a2bd506927dcb4bfe60
Keigo-Iwakuma <sym90chn@gmail.com>
2022-08-16 23:53:45 +0900

    update sampling.py.

---
+++ sampling.py
@@ -0,0 +1,137 @@
+"""Various sampling methods."""
+import abc
+import functools
+from threading import local
+
+import torch
+import numpy as np
+from scipy import integrate
+
+from models.utils import from_flattened_numpy, to_flattend_numpy, get_score_fn
+from models import utils as mutils
+import sde_lib
+
+
+_CORRECTORS = {}
+_PREDICTORS = {}
+
+
+def register_predictor(cls=None, *, name=None):
+    """A decorator for registering predictor classes."""
+
+    def _register(cls):
+        if name is None:
+            local_name = cls.__name__
+        else:
+            local_name = name
+        if local_name in _PREDICTORS:
+            raise ValueError(f"Already registered model with name: {local_name}")
+        _PREDICTORS[local_name] = cls
+        return cls
+    
+    if cls is None:
+        return _register
+    else:
+        return _register(cls)
+
+
+def register_corrector(cls=None, *, name=None):
+    """A decorator for registering corrector classes."""
+
+    def _register(cls):
+        if name is None:
+            local_name = cls.__name__
+        else:
+            local_name = name
+        if local_name in _CORRECTORS:
+            raise ValueError(f"Already registered model with name: {local_name}")
+        _CORRECTORS[local_name] = cls
+        return cls
+    
+    if cls is None:
+        return _register
+    else:
+        return _register(cls)
+
+
+def get_predictor(name):
+    return _PREDICTORS[name]
+
+
+def get_corrector(name):
+    return _CORRECTORS[name]
+
+
+def get_sampling_fn(config, sde, shape, inverse_scaler, eps):
+    """
+    Create a sampling function.
+
+    Args:
+        config: A `ml_collections.ConfigDict` object that contains all configuration information.
+        sde: A `sde_lib.SDE` object that represents the forward SDE.
+        shape: A sequence of integers representing the expected shape of a single sample.
+        inverse_scaler: The inverse data normalizer function.
+        eps: A `float` number. The reverse-time SDE is only integrated to `eps` for nummerical stability.
+    
+    Returns:
+        A function that takes random states and a replicated training state and outputs samples with the
+        trailing dimentions matching `shape`.
+    """
+
+    sampler_name = config.sampling.method
+    # Probalility flow ODE sampling with black-box ODE solvers
+    if sampler_name.lower() == "ode":
+        sampling_fn = get_ode_sampler(
+            sde=sde,
+        )
+
+class Predictor(abc.ABC):
+    """The abstract class for a predictor algorithm."""
+
+    def __init__(self, sde, score_fn, probability_flow=False):
+        super().__init__()
+        self.sde = sde
+        # Compute the reverse SDE/ODE
+        self.rsde = sde.reverse(score_fn, probability_flow)
+        self.score_fn = score_fn
+    
+    @abc.abstractmethod
+    def update_fn(self, x, t):
+        """
+        One update of the predictor.
+
+        Args:
+            x: A PyTorch tensor representing the current state.
+            t: A PyTorch tensor representing the current time step.
+
+        Returns:
+            x: A PyTorch tensor of the next state.
+            x_mean: A PyTorch tensor. The next state without random noise. Useful for denoising.
+        """
+        pass
+
+
+class Corrector(abc.ABC):
+    """The abstract class for a corrector algorithm."""
+
+    def __init__(self, sde, score_fn, snr, n_steps):
+        super().__init__()
+        self.sde = sde
+        self.score_fn = score_fn
+        self.snr = snr
+        self.n_steps = n_steps
+    
+    @abc.abstractmethod
+    def update_fn(self, x, t):
+        """
+        One update of the corrector.
+
+        Args:
+            x: A PyTorch tensor representing the current state
+            t: A PyTorch tensor representing the current time step.
+
+        Returns:
+            x: A PyTorch tensor of the next state.
+            x_mean: A PyTorch tensor. The next state without random noise. Useful for denoising.
+        """
+        pass
