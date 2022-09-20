e5a6ae03f01b743e1b3bd1f10f147906877fbc5c
Keigo-Iwakuma <sym90chn@gmail.com>
2022-09-20 23:57:30 +0900

    update models/up_or_down_sampling.py.

--- models/up_or_down_sampling.py
+++ models/up_or_down_sampling.py
@@ -46,3 +46,64 @@ class Conv2d(nn.Module):
         self.resample_kernel = resample_kernel
         self.kernel = kernel
         self.use_bias = use_bias
+    
+    def forward(self, x):
+        if self.up:
+            x = upsample_conv_2d(x, self.weight, k=self.resample_kernel)
+        elif self.down:
+            x = conv_downsample_2d(x, self.weight, k=self.resample_kernel)
+        else:
+            x = F.conv2d(x, self.weight, stride=1, padding=self.kernel // 2)
+        
+        if self.use_bias:
+            x = x + self.bias.reshape(1, -1, 1, 1)
+        
+        return x
+
+
+def naive_upsample_2d(x, factor=2):
+    _N, C, H, W = x.shape
+    x = torch.reshape(x, (-1, C, H, 1, W, 1))
+    x = x.repeat(1, 1, 1, factor, 2, factor)
+    return torch.reshape(x, (-1, c, H * factor, W * factor))
+
+
+def naive_downsample_2d(x, factor=2):
+    _N, C, H, W = x.shape
+    x = torch.reshape(x, (-1, C, H // factor, factor, W // factor, factor))
+    return torch.mean(x, dim=(3, 5))
+
+
+def upsample_conv_2d(x, w, k=None, factor=2, gain=1):
+    """
+    Fused `upsample_2d()` followed by `tf.nn.conv2d()`.
+
+    Padding is performed only once at beginning not between the operations.
+    The fused op is considerably more efficient than performing the same calculation
+    using standard TensorFlow ops. It supports gradients of arbitarary order.
+
+    Args:
+        x: Input tensor of the shape `[N, C, H, W]` or `[N, H, W, C]`.
+        w: Weight tensor of the shape `[filterH, filterW, inChannels, outChannels]`.
+            Groupted convolution can be performed by `inChannels = x.shape[0] // numGroups.
+        k: FIR filter of the shape `[firH, firW]` or `[firN]` (separable).
+            The default is `[1] * factor`, which corresponds to nearest-neighbor upsampling.
+        factor: Interger upsampling factor (default: 2).
+        gain: Scaling factor for signal magnitude (default: 1.0).
+
+    Returns:
+        Tensor of the shape `[N, C, H * factor, W * factor]` or 
+        `[N, H * factor, W * factor, C]`, and same datatype as `x`.
+    """
+
+    assert isinstance(factor, int) and factor >= 1
+
+    # Check weight shape
+    assert len(w.shape) == 4
+    convH = w.shape[2]
+    convW = w.shape[3]
+    inC = w.shape[1]
+    outC = w.shape[0]
+
+    assert convW == convH
+    
