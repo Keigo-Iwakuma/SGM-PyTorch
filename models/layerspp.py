9b2dfbab7f8e5c518959dcd2402c83c569971931
Keigo-Iwakuma <sym90chn@gmail.com>
2022-10-05 00:00:01 +0900

    update

--- models/layerspp.py
+++ models/layerspp.py
@@ -97,9 +97,15 @@ class Upsample(nn.Module):
         else:
             if with_conv:
                 self.Conv2d_0 = up_or_down_sampling.Conv2d(
-                    in_ch, out_ch, kernel=3, up=True, resample_kernel=fir_kernel, use_bias=True, kernel_init=default_init()
+                    in_ch,
+                    out_ch,
+                    kernel=3,
+                    up=True,
+                    resample_kernel=fir_kernel,
+                    use_bias=True,
+                    kernel_init=default_init(),
                 )
-        self.fir = fir   
+        self.fir = fir
         self.with_conv = with_conv
         self.fir_kernel = fir_kernel
         self.out_ch = out_ch
@@ -115,12 +121,19 @@ class Upsample(nn.Module):
                 h = up_or_down_sampling.upsample_2d(x, self.fir_kernel, factor=2)
             else:
                 h = self.Conv2d_0(x)
-        
+
         return h
 
 
 class Downsample(nn.Module):
-    def __init__(self, in_ch=None, out_ch=None, with_conv=False, fir=False, fir_kernel=(1, 3, 3, 1)):
+    def __init__(
+        self,
+        in_ch=None,
+        out_ch=None,
+        with_conv=False,
+        fir=False,
+        fir_kernel=(1, 3, 3, 1),
+    ):
         super().__init__()
         out_ch = out_ch if out_ch else in_ch
         if not fir:
@@ -128,16 +141,20 @@ class Downsample(nn.Module):
                 self.Conv_0 = conv3x3(in_ch, out_ch, stride=2, padding=0)
         else:
             if with_conv:
-                self.Conv2d_0 = up_or_down_sampling.Conv2d(in_ch, out_ch,
-                kernel=3, down=True,
-                resample_kernel=fir_kernel,
-                use_bias=True, 
-                kernel_init=default_init())
+                self.Conv2d_0 = up_or_down_sampling.Conv2d(
+                    in_ch,
+                    out_ch,
+                    kernel=3,
+                    down=True,
+                    resample_kernel=fir_kernel,
+                    use_bias=True,
+                    kernel_init=default_init(),
+                )
         self.fir = fir
         self.fir_kernel = fir_kernel
         self.with_conv = with_conv
         self.out_ch = out_ch
-    
+
     def forward(self, x):
         B, C, H, W = x.shape
         if not self.fir:
@@ -151,25 +168,38 @@ class Downsample(nn.Module):
                 x = up_or_down_sampling.downsample_2d(x, self, fir_kernel, factor=2)
             else:
                 x = self.Conv2d_0(x)
-        
+
         return x
 
 
 class ResNetBlockDDPMpp(nn.Module):
     """ResBlock adapted from DDPM."""
 
-    def __init__(self, act, in_ch, out_ch=None, temb_dim=None, conv_shortcut=False,
-    dropout=0.1, skip_rescale=False, init_scale=0.):
+    def __init__(
+        self,
+        act,
+        in_ch,
+        out_ch=None,
+        temb_dim=None,
+        conv_shortcut=False,
+        dropout=0.1,
+        skip_rescale=False,
+        init_scale=0.0,
+    ):
         super().__init__()
         out_ch = out_ch if out_ch else in_ch
-        self.GroupNorm_0 = nn.GroupNorm(num_groups=min(in_ch // 4, 32), num_channels=in_ch, eps=1e-6)
+        self.GroupNorm_0 = nn.GroupNorm(
+            num_groups=min(in_ch // 4, 32), num_channels=in_ch, eps=1e-6
+        )
         self.Conv_0 = conv3x3(in_ch, out_ch)
         if temb_dim is not None:
             self.Dense_0 = nn.Linear(temb_dim, out_ch)
 
             self.Dense_0.weight.data = default_init()(self.Dense_0.weight.data.shape)
             nn.init.zeros_(self.Dense_0.bias)
-        self.GroupNorm_1 = nn.GroupNorm(num_groups=min(out_ch, // 4, 32), num_channels=out_ch, eps=1e-6)
+        self.GroupNorm_1 = nn.GroupNorm(
+            num_groups=min(out_ch // 4, 32), num_channels=out_ch, eps=1e-6
+        )
         self.Dropout_0 = nn.Dropout(dropout)
         self.Conv_1 = conv3x3(out_ch, out_ch, init_scale=init_scale)
         if in_ch != out_ch:
@@ -177,9 +207,66 @@ class ResNetBlockDDPMpp(nn.Module):
                 self.Conv_2 = conv3x3(in_ch, out_ch)
             else:
                 self.NIN_0 = NIN(in_ch, out_ch)
-        
+
         self.skip_rescale = skip_rescale
         self.act = act
         self.out_ch = out_ch
         self.conv_shortcut = conv_shortcut
     
+    def forward(self, x, temb=None):
+        h = self.act(self.GroupNorm_0(x))
+        h = self.Conv_0(h)
+        if temb is not None:
+            h += self.Dense_0(self.act(temb))[:, :, None, None]
+        h = self.act(self.GroupNorm_1(h))
+        h = self.Dropout_0(h)
+        h = self.Conv_1(h)
+        if x.shape[1] != self.out_ch:
+            if self.conv_shortcut:
+                x = self.Conv_2(x)
+            else:
+                x = self.NIN_0(x)
+        if not self.skip_rescale:
+            return x + h
+        else:
+            return (x + h) / np.sqrt(2.)
+
+
+class ResnetBlockBigGANpp(nn.Module):
+    def __init__(
+        self,
+        act,
+        in_ch,
+        out_ch=None,
+        temb_dim=None, 
+        up=False,
+        down=False,
+        dropout=0.1,
+        fir=False,
+        fir_kernel=(1, 3, 3, 1),
+        skip_rescale=True,
+        init_scale=0.,
+    ):
+        super().__init__()
+        self.GroupNorm_0 = nn.GroupNorm(num_groups=min(in_ch // 4, 32), numj_channels=in_ch, eps=1e-6)
+        self.up = up
+        self.down = down
+        self.fir = fir
+        self.fir_kernel = fir_kernel
+
+        self.Conv_0 = conv3x3(in_ch, out_ch)
+        if temb_dim is not None:
+            self.Dense_0 = nn.Linear(temb_dim, out_ch)
+            self.Dense_0.weight.data = default_init()(self.Dense_0.weight.shape)
+            nn.init.zeros_(self.Dense_0.bias)
+
+        self.GroupNorm_1 = nn.GroupNorm(num_groups=min(out_ch // 4, 32), num_channels=out_ch, eps=1e-6)
+        self.Dropout_0 = nn.Dropout(dropout)
+        self.Conv_1 = conv3x3(out_ch, out_ch, init_scale=init_scale)
+        if in_ch != out_ch or up or down:
+            self.Conv_2 = conv1x1(in_ch, out_ch)
+        
+        self.skip_rescale = skip_rescale
+        self.act = act
+        self.in_ch = in_ch
+        self.out_ch = out_ch
